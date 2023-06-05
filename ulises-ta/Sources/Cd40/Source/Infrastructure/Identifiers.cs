6c93e5b3ce4d142414d00c0a5cdb7ba75c01642c
Eduardo <Eduardo@b1d50d5f-df8f-9249-af92-6a0d583c2fd7>
2023-06-05 14:32:46 +0000

    Merged revision(s) 12045 from Ulises5000I/Desarrollos/Desarrollo_Version_3.0.0/eduSp23_3/ulises-ta: Se corrige archivo Identifiers.cs ........
    git-svn-id: http://10.34.2.66/svn/svn_repository/Ulises5000I/branches/u5kiv.3.0.0@12046 b1d50d5f-df8f-9249-af92-6a0d583c2fd7

--- ulises-ta/Sources/Cd40/Source/Infrastructure/Identifiers.cs
+++ ulises-ta/Sources/Cd40/Source/Infrastructure/Identifiers.cs
@@ -8,7 +8,7 @@ namespace U5ki.Infrastructure
     /// 
     /// </summary>
 	public static class Identifiers
-	{
+    {
         /// <summary>
         /// 
         /// </summary>
@@ -67,12 +67,11 @@ namespace U5ki.Infrastructure
 
 
 
-		public const short FR_RXTX_CHANGE_ASK_MSG = 5;//LALM 221102 cambiofrecuencia
+        public const short FR_RXTX_CHANGE_ASK_MSG = 5;//LALM 221102 cambiofrecuencia
         /// <summary>
         /// 
         /// </summary>
 		public const short FR_RX_CHANGE_RESPONSE_MSG = 51;
-
         /// <summary>
         /// 
         /// </summary>
@@ -85,20 +84,11 @@ namespace U5ki.Infrastructure
         /// 
         /// </summary>
         public const short FR_HF_TX_CHANGE_RESPONSE_MSG = 54;
-		public const short FR_RXTX_CHANGE_RESPONSE_MSG = 55;//LALM 221102 cambiofrecuencia
-
+        public const short FR_RXTX_CHANGE_RESPONSE_MSG = 55;//LALM 221102 cambiofrecuencia
         /// <summary>
         /// Codigos de retorno para el mensaje de cambio de frecuencia FrChangeRsp, asociado a FR_RXTX_CHANGE_RESPONSE_MSG
         /// </summary>
-        public const uint FR_CHANGE_OK = 500;       //Cambio de frecuencia OK
-        public const uint FR_IS_IN_USE = 501;       //La frecuencia esta seleccionada en algun puesto
-        public const uint FR_IN_PROGRESS = 502;     //Otro cambio de frecuencia esta en proceso
-        public const uint FR_GENERIC_ERROR = 506;   //Otros errores
-        public const uint FR_EQ_NO_RESPOND = 503;   //El equipo radio no responde
-        public const uint FR_CH_REJECTED = 504;     //El cambio de frecuencia es rechazado por el equipo radio
-        public const uint FR_INCORRECT_FREQ = 505;  //La frecuencia resultante no es la correcta
-                                                    //
-                                                    
+
         // Mensajes referentes a las conferencias preprogramadas
         public const short CONFERENCE_STATUS = 520;  //Estado de la conferencia. Se envia mensaje ConferenceStatus de TopMessages.proto
 
@@ -142,22 +132,35 @@ namespace U5ki.Infrastructure
         public const short IM_MASTER_MSG = 99;
 
         /// <summary>
+        /// Codigos de retorno para el mensaje de cambio de frecuencia FrChangeRsp, asociado a FR_RXTX_CHANGE_RESPONSE_MSG
+        /// </summary>
+        public const uint FR_CHANGE_OK = 500;       //Cambio de frecuencia OK
+        public const uint FR_IS_IN_USE = 501;       //La frecuencia esta seleccionada en algun puesto
+        public const uint FR_IN_PROGRESS = 502;     //Otro cambio de frecuencia esta en proceso
+        public const uint FR_EQ_NO_RESPOND = 503;   //El equipo radio no responde
+        public const uint FR_CH_REJECTED = 504;     //El cambio de frecuencia es rechazado por el equipo radio
+        public const uint FR_INCORRECT_FREQ = 505;  //La frecuancia resultante no es la correcta
+        public const uint FR_GENERIC_ERROR = 506;   //Otros errores
+        public const uint FR_TIMEOUT_ERROR = 507;   //No se ha recibido respuesta
+
+
+        /// <summary>
         /// 
         /// </summary>
         /// <param name="t"></param>
         /// <returns></returns>
 		public static string TypeId(Type t)
-		{
-			return t.Namespace + "." + t.Name;
-		}
+        {
+            return t.Namespace + "." + t.Name;
+        }
         /// <summary>
         /// 
         /// </summary>
         /// <param name="typeId"></param>
         /// <returns></returns>
 		public static Type GetType(string typeId)
-		{
-			return Type.GetType(typeId);
-		}
-	}
+        {
+            return Type.GetType(typeId);
+        }
+    }
 }
