c111858d5c48ecd38221b54ef4e04a42e7b5cee8
Arturo <Arturo@b1d50d5f-df8f-9249-af92-6a0d583c2fd7>
2023-11-23 14:52:20 +0000

    TA. Etiquetado 3.0.0.3
    git-svn-id: http://10.34.2.66/svn/svn_repository/Ulises5000I/branches/u5kiv.3.0.0@12390 b1d50d5f-df8f-9249-af92-6a0d583c2fd7

--- ulises-ta/Sources/Cd40/Source/VersionInfo.cs
+++ ulises-ta/Sources/Cd40/Source/VersionInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // Puede especificar todos los valores o establecer como predeterminados los números de compilación y de revisión 
 // mediante el carácter '*', como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.0.1")]
-[assembly: AssemblyFileVersion("3.0.0.1")]
+[assembly: AssemblyVersion("3.0.0.3")]
+[assembly: AssemblyFileVersion("3.0.0.3")]
--- ulises-ta/Sources/TFT/HMI/Source/VersionInfo.cs
+++ ulises-ta/Sources/TFT/HMI/Source/VersionInfo.cs
@@ -33,5 +33,5 @@ using System.Reflection;
 // Puede especificar todos los valores o establecer como predeterminados los números de compilación y de revisión 
 // mediante el carácter '*', como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.0.1")]
-[assembly: AssemblyFileVersion("3.0.0.1")]
+[assembly: AssemblyVersion("3.0.0.3")]
+[assembly: AssemblyFileVersion("3.0.0.3")]
