0ea73cefa71d03c4551fcd2bfd16645e91a68f49
Eduardo Pascual <eduardo.pascual@grupoamper.com>
2024-01-31 15:40:11 +0100

    11493 NBX-RADIO-GW. Diseño de un simulador radio SIRTAP. Se muestran los parametros SNMP

---
+++ ulises-ta/Sources/Cd40/SirtapRadio/FormSnmpData.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace SirtapRadio
+{
+    partial class FormSnmpData
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_numChannels = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_ChannelSelected = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_FrequencyCh1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_FrequencyCh2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox_PowerLevelCh1 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox_PowerLevelCh2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox_TxInhibit = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox_WFCh1 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox_WFCh2 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button_Save = new System.Windows.Forms.Button();
+            this.button_Close = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox_numChannels
+            // 
+            this.textBox_numChannels.Location = new System.Drawing.Point(191, 24);
+            this.textBox_numChannels.Name = "textBox_numChannels";
+            this.textBox_numChannels.Size = new System.Drawing.Size(244, 26);
+            this.textBox_numChannels.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "numChannels";
+            // 
+            // textBox_ChannelSelected
+            // 
+            this.textBox_ChannelSelected.Location = new System.Drawing.Point(191, 56);
+            this.textBox_ChannelSelected.Name = "textBox_ChannelSelected";
+            this.textBox_ChannelSelected.Size = new System.Drawing.Size(244, 26);
+            this.textBox_ChannelSelected.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "ChannelSelected";
+            // 
+            // textBox_FrequencyCh1
+            // 
+            this.textBox_FrequencyCh1.Location = new System.Drawing.Point(191, 88);
+            this.textBox_FrequencyCh1.Name = "textBox_FrequencyCh1";
+            this.textBox_FrequencyCh1.Size = new System.Drawing.Size(244, 26);
+            this.textBox_FrequencyCh1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "FrequencyCh1";
+            // 
+            // textBox_FrequencyCh2
+            // 
+            this.textBox_FrequencyCh2.Location = new System.Drawing.Point(191, 120);
+            this.textBox_FrequencyCh2.Name = "textBox_FrequencyCh2";
+            this.textBox_FrequencyCh2.Size = new System.Drawing.Size(244, 26);
+            this.textBox_FrequencyCh2.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 120);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "FrequencyCh2";
+            // 
+            // textBox_PowerLevelCh1
+            // 
+            this.textBox_PowerLevelCh1.Location = new System.Drawing.Point(191, 152);
+            this.textBox_PowerLevelCh1.Name = "textBox_PowerLevelCh1";
+            this.textBox_PowerLevelCh1.Size = new System.Drawing.Size(244, 26);
+            this.textBox_PowerLevelCh1.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(27, 152);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(119, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "PowerLevelCh1";
+            // 
+            // textBox_PowerLevelCh2
+            // 
+            this.textBox_PowerLevelCh2.Location = new System.Drawing.Point(191, 184);
+            this.textBox_PowerLevelCh2.Name = "textBox_PowerLevelCh2";
+            this.textBox_PowerLevelCh2.Size = new System.Drawing.Size(244, 26);
+            this.textBox_PowerLevelCh2.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(27, 184);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(119, 20);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "PowerLevelCh2";
+            // 
+            // textBox_TxInhibit
+            // 
+            this.textBox_TxInhibit.Location = new System.Drawing.Point(191, 216);
+            this.textBox_TxInhibit.Name = "textBox_TxInhibit";
+            this.textBox_TxInhibit.Size = new System.Drawing.Size(244, 26);
+            this.textBox_TxInhibit.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(27, 216);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(68, 20);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "TxInhibit";
+            // 
+            // textBox_WFCh1
+            // 
+            this.textBox_WFCh1.Location = new System.Drawing.Point(191, 248);
+            this.textBox_WFCh1.Name = "textBox_WFCh1";
+            this.textBox_WFCh1.Size = new System.Drawing.Size(244, 26);
+            this.textBox_WFCh1.TabIndex = 14;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(27, 248);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(63, 20);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "WFCh1";
+            // 
+            // textBox_WFCh2
+            // 
+            this.textBox_WFCh2.Location = new System.Drawing.Point(191, 280);
+            this.textBox_WFCh2.Name = "textBox_WFCh2";
+            this.textBox_WFCh2.Size = new System.Drawing.Size(244, 26);
+            this.textBox_WFCh2.TabIndex = 16;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(27, 280);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(63, 20);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "WFCh2";
+            // 
+            // button_Save
+            // 
+            this.button_Save.Location = new System.Drawing.Point(31, 322);
+            this.button_Save.Name = "button_Save";
+            this.button_Save.Size = new System.Drawing.Size(149, 49);
+            this.button_Save.TabIndex = 18;
+            this.button_Save.Text = "Save";
+            this.button_Save.UseVisualStyleBackColor = true;
+            this.button_Save.Click += new System.EventHandler(this.button_Save_Click);
+            // 
+            // button_Close
+            // 
+            this.button_Close.Location = new System.Drawing.Point(215, 322);
+            this.button_Close.Name = "button_Close";
+            this.button_Close.Size = new System.Drawing.Size(142, 49);
+            this.button_Close.TabIndex = 19;
+            this.button_Close.Text = "Close";
+            this.button_Close.UseVisualStyleBackColor = true;
+            this.button_Close.Click += new System.EventHandler(this.button_Close_Click);
+            // 
+            // FormSnmpData
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(488, 391);
+            this.Controls.Add(this.button_Close);
+            this.Controls.Add(this.button_Save);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox_WFCh2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox_WFCh1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox_TxInhibit);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox_PowerLevelCh2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox_PowerLevelCh1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox_FrequencyCh2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox_FrequencyCh1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_ChannelSelected);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_numChannels);
+            this.Name = "FormSnmpData";
+            this.Text = "SNMP PARAMETERS";
+            this.Load += new System.EventHandler(this.FormSnmpData_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_numChannels;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_ChannelSelected;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_FrequencyCh1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_FrequencyCh2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox_PowerLevelCh1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox_PowerLevelCh2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox_TxInhibit;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox_WFCh1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox_WFCh2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button_Save;
+        private System.Windows.Forms.Button button_Close;
+    }
+}
